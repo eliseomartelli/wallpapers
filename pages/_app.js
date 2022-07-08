dac7e2f5a7fd6e9a20ddb6ab85065cd66725979b
Eliseo Martelli <me@eliseomartelli.it>
2022-07-09 00:01:04 +0200

    feat: add back arrow

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,11 @@ import "../styles/globals.css";
 import logo from "../public/logo.svg";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/router";
+
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
   return (
     <div className="flex flex-col gap-4">
       <header className="grow shadow-lg 2 p-4 font-bold flex justify-center">
@@ -11,6 +14,11 @@ function MyApp({ Component, pageProps }) {
           <Link href="/" passHref>
             <a>
               <div className="flex gap-4 select-none cursor-pointer">
+                {router.asPath !== "/" &&(
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
+                  </svg>
+                )}
                 <Image src={logo} width={24} height={24} />
                 <p>Wallpapers</p>
               </div>
